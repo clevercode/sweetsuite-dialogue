14e17e382ecfdc484cdf22731925b6c9e707360b
Andrew  Smith <andrew@envylabs.com>
2011-01-12 19:49:09 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dialogue::Application.config.secret_token = 'd8b0df2ba00576c19fe4a3b5a9674a871908ca2740d89023f599f2bd63fa1ccb5dc293bda8fd782be6a2bc8a0d3b02ce832a688319c1ede4c9017c8fd83bc1c4'
