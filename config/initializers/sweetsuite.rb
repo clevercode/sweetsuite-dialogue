6fd0c16c79689a4c24c58aefbb20b75cc9632764
Andrew  Smith <andrew@envylabs.com>
2011-01-15 15:41:53 -0500

    Initial SweetSuite gem configuration.

---
+++ config/initializers/sweetsuite.rb
@@ -0,0 +1,11 @@
+require 'yaml'
+
+begin
+  configuration = YAML.load_file(Rails.root.join('config','sweetsuite.yml')
+  configuration = configuration[Rails.env]
+  configuration = HashWithIndifferentAccess.new(configuration)
+  SweetSuite.config.update(configuration)
+
+rescue LoadError
+  puts "The /config/sweetsuite.yml file is missing or broken"
+end
