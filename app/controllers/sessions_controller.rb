73661931ab2cb884d21595ff2a1972cf04e7db18
Olivier Lacan <olivier.lacan@gmail.com>
2011-01-16 01:10:25 -0500

    Handle OmniAuth callback through the session controller.
    Signed-off: Andrew Smith

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+
+  def create
+    session[:access_token] = auth_hash['credentials']['token']
+    redirect_to root_url
+  end
+  
+  private
+
+  def auth_hash
+    env['omniauth.auth']
+  end
+
 end
