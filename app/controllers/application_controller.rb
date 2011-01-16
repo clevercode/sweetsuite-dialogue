f4496df7f5de94ab58944684f5c3733d4d0b8f46
Andrew  Smith <andrew@envylabs.com>
2011-01-16 17:11:42 -0500

    Extracted SweetSuite helpers in ApplicationController to SweetSuite gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-
-  helper_method :current_user
-
-  protected
-
-  def authenticate_user!
-    redirect_to '/auth/sweetsuite' unless access_token
-  end
-
-  def access_token
-    session[:access_token]
-  end
-
-  def current_user
-    @user ||= MultiJson.decode(ss_api.get('profile.json'))['user']
-  end
-
-  def ss_api
-    @ss_api ||= OAuth2::AccessToken.new(ss_client, access_token) 
-  end
-  
-  def ss_client
-    @ss_client ||= OAuth2::Client.new(SweetSuite.config.app_key, SweetSuite.config.app_secret, {:site => SweetSuite.config.auth_server} )
-  end
+  include SweetSuite::Helpers
 end
